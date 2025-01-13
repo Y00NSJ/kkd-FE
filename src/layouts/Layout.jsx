75985db18dde48a3c7c1914def2205fad15e42ba
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-14 02:36:50 +0900

    Fix: 레이아웃을 전역에서 사용하기 위한 상태 정의

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import Stars from '../components/Stars';
-import Clouds from '../components/Clouds';
+import React from "react";
+import Navbar from "../components/Navbar";
+import Stars from "../components/Stars";
+import Clouds from "../components/Clouds";
+import { useState, useEffect } from "react";
 
-const Layout = ({isDarkMode, toggleDarkMode, children}) => {
-    return (
-      <div className={`relative min-h-screen overflow-hidden ${
-      isDarkMode
-      ? 'bg-gradient-to-b from-purple-950 via-violet-800 to-blue-900'
-      : 'bg-gradient-to-b from-purple-400 via-pink-300 to-blue-300'}`}>
-        <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>
-        <Stars />
-        <Clouds />
-        {children}
-      </div>
-    );
-  }
-  
-  export default Layout;
+const Layout = ({ isDarkMode, toggleDarkMode, children }) => {
+  const [backgroundClass, setBackgroundClass] = useState("");
+  useEffect(() => {
+    const bgClass = isDarkMode
+      ? "bg-gradient-to-b from-purple-950 via-violet-800 to-blue-900"
+      : "bg-gradient-to-b from-purple-400 via-pink-300 to-blue-300";
+    setBackgroundClass(bgClass);
+  }, [isDarkMode]);
+
+  return (
+    <div className={`relative min-h-screen overflow-hidden ${backgroundClass}`}>
+      <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
+      <Stars />
+      <Clouds />
+      {React.Children.map(children, (child) =>
+        React.cloneElement(child, { backgroundClass })
+      )}
+    </div>
+  );
+};
+
+export default Layout;
