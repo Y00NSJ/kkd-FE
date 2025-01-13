82562e87df1b9d5514371084ce61ca440942d3b0
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-13 22:13:45 +0900

    UI: 로그인 버튼 디자인

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Stars from '../components/Stars';
 import Clouds from '../components/Clouds';
+import '../styles/LoginButton.css'
 
 function HomePage() {
   return (
@@ -14,11 +15,13 @@ function HomePage() {
           Create Your Dream World
         </h1>
         <div className="flex flex-col gap-6 items-center">
-          <button className="bg-black/90 hover:bg-custom text-white px-12 py-4 rounded-full text-xl font-semibold transition-all transform hover:scale-105 hover:shadow-lg !rounded-button">
+          <button className="bg-black/10 hover:bg-custom text-white px-12 py-4 rounded-full text-xl font-semibold transition-all transform hover:scale-105 hover:shadow-lg !rounded-button">
             Sign Up
           </button>
-          <button className="bg-white/90 hover:bg-white text-custom px-12 py-4 rounded-full text-xl font-semibold transition-all transform hover:scale-105 hover:shadow-lg !rounded-button">
-            Login
+        </div>
+        <div className="flex flex-col gap-6 items-center">
+          <button className="hover:shadow-lg px-12 py-4 hover:scale-105 transition-all login-button">
+            <span>Login</span>
           </button>
         </div>
       </main>
