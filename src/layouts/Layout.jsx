907e8154f05b89bc5295ccabc5cf068c9c2406f2
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-14 15:41:35 +0900

    UI: 구름 svg 추가

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -8,19 +8,21 @@ const Layout = ({ isDarkMode, toggleDarkMode, children }) => {
   const [backgroundClass, setBackgroundClass] = useState("");
   useEffect(() => {
     const bgClass = isDarkMode
-      ? "bg-gradient-to-b from-purple-950 via-violet-800 to-blue-900"
+      ? "bg-gradient-to-b from-purple-950 via-violet-800 to-blue-800"
       : "bg-gradient-to-b from-purple-400 via-pink-300 to-blue-300";
     setBackgroundClass(bgClass);
   }, [isDarkMode]);
 
   return (
-    <div className={`relative min-h-screen overflow-hidden ${backgroundClass}`}>
+    <div
+      className={`relative min-h-screen overflow-hidden ${
+        backgroundClass || ""
+      }`}
+    >
       <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
       <Stars />
       <Clouds />
-      {React.Children.map(children, (child) =>
-        React.cloneElement(child, { backgroundClass })
-      )}
+      {children}
     </div>
   );
 };
