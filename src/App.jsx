39a969489741b533ee6841d39824b2e162b5a285
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-14 23:12:19 +0900

    Chore: 전역 css 정의 및 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CreateDreamPage from "./pages/CreateDreamPage";
+import PlayVideoPage from "./pages/PlayVideoPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/dreams/create/" element={<CreateDreamPage />} />
+        <Route path="/dreams/view/" element={<PlayVideoPage />} />
       </Routes>
     </Router>
   );
