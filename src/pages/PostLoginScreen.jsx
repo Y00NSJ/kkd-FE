9b844851b7ee3b940b644282d55894072c81524b
Y00NSJ <ysj0509@sookmyung.ac.kr>
2025-01-15 00:57:14 +0900

    Feat: 임시 메인 화면 추가

--- src/pages/PostLoginScreen.jsx
+++ src/pages/PostLoginScreen.jsx
@@ -1,5 +1,35 @@
+import { ThemeProvider, useTheme } from "../contexts/ThemeContext.jsx";
+import Layout from "../layouts/Layout";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 const PostLoginScreen = ({ onLogout }) => {
-  return console.log("done");
+  const { isDarkMode, toggleDarkMode } = useTheme();
+  const navigate = useNavigate();
+
+  return (
+    <Layout isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}>
+      <div className="flex items-center justify-center min-h-screen">
+        {/* DreamCreate Button */}
+        <button
+          onClick={() => {
+            console.log("이동");
+            navigate("/dreams/create/");
+          }}
+          className="btn-primary"
+        >
+          Dream Create
+        </button>
+        {/* Logout Button */}
+        <button
+          onClick={onLogout}
+          className="px-6 py-3 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600"
+        >
+          Logout
+        </button>
+      </div>
+    </Layout>
+  );
 };
 
 export default PostLoginScreen;
